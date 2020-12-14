78ee81283165f6edbfe77337676f45f223fe5037
Vanessa Bergen <vanessa.bergen1@gmail.com>
2020-12-14 12:48:37 -0500

    Adds websockets functionality to server.js
    Adds code to listen for clients connecting
    Sends a message via websockets to connecting clients

--- server/server.js
+++ server/server.js
@@ -5,3 +5,23 @@ var app = express();
 var server = app.listen(4000, function() {
 	console.log('listening to requests on port 4000');
 });
+
+var webSocketServer = new WebSocket.Server({ server: server });
+webSocketServer.on('connection', function connection(ws) {
+	console.log('client connected');
+	ws.send("Hello from server");
+
+	ws.on('message', function incoming(data) {
+		console.log('Server Received: %s', data);
+	});
+
+});
+
+// telling it to use static files in the public folder
+app.use(express.static('public'));
+
+// Set Express routes
+// Setting the home page of web server
+app.get('/', (req, res) => {
+  	res.sendFile(__dirname + '/public/views/chat.html');
+});
