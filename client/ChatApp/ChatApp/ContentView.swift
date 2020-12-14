f542fb16e31b10e84fabdf77c19fe20909f8ae0c
Vanessa Bergen <vanessa.bergen1@gmail.com>
2020-12-14 13:31:27 -0500

    Add view to send and recevied messages

--- client/ChatApp/ChatApp/ContentView.swift
+++ client/ChatApp/ChatApp/ContentView.swift
@@ -9,8 +9,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var service = SocketManager()
+    @State private var output = ""
+    @State private var handle = ""
+    @State private var message = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        GeometryReader { geo in
+            VStack {
+                ScrollView {
+                    ForEach(0..<10) { num in
+                        HStack {
+                            Text("\(num): ")
+                                .bold()
+                            Text("message here")
+                            Spacer()
+                        }
+                        .padding([.leading, .trailing], 5)
+                        .padding([.top, .bottom], 1)
+                        
+                    }.frame(maxWidth: .infinity)
+                        
+                }
+                .border(Color.purple)
+                .padding()
+                    
+                    
+                TextField("Handle", text: self.$handle)
+                    .border(Color.purple)
+                    .frame(width: 0.9 * geo.size.width)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                TextField("Message", text: self.$message)
+                    .border(Color.purple)
+                    .frame(width: 0.9 * geo.size.width)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                Button(action: {
+                    // Send message here
+                    self.service.send(with: self.handle, for: self.message)
+                }) {
+                    Text("Send!")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .padding()
+                }
+                .background(Color.purple)
+                .cornerRadius(25)
+                .padding(.bottom)
+                
+            }
+        }
     }
 }
 
