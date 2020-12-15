5e06c305f0c3ba3942fab67f05be1531b36d6d3b
Vanessa Bergen <vanessa.bergen1@gmail.com>
2020-12-15 11:54:46 -0500

    Imports UUID package to frontend

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var WebSocket = require('ws');
+const {v4 : uuidv4} = require('uuid'); 
 
 var app = express();
 var server = app.listen(4000, function() {
@@ -7,18 +8,26 @@ var server = app.listen(4000, function() {
 });
 
 var webSocketServer = new WebSocket.Server({ server: server });
+webSocketServer.binaryType = 'arraybuffer';
+
 webSocketServer.on('connection', function connection(ws) {
 	console.log('client connected');
 	ws.send("Hello from server");
 
 	ws.on('message', function incoming(data) {
 		console.log('Server Received: %s', data);
+		webSocketServer.clients.forEach(function each(client) {
+			client.send(data);
+		});
 	});
 
 });
 
 // telling it to use static files in the public folder
 app.use(express.static('public'));
+
+// creating a static route to the uuid module
+app.use('/scripts', express.static(__dirname + '/node_modules/uuid/dist/'));
 
 // Set Express routes
 // Setting the home page of web server
