e6d0ec5a67f32121e62d5e681068c88fd5be5272
Vanessa Bergen <vanessa.bergen1@gmail.com>
2020-12-15 19:31:49 -0500

    initializes the express app and database

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
-var express = require('express');
+var mongoScript = require('./config/mongoose.js');
+var expressScript = require('./config/express.js');
 var WebSocket = require('ws');
 const {v4 : uuidv4} = require('uuid'); 
 
-var app = express();
+var db = mongoScript();
+var app = expressScript();
+
+//var app = express();
 var server = app.listen(4000, function() {
 	console.log('listening to requests on port 4000');
 });
@@ -12,7 +16,6 @@ webSocketServer.binaryType = 'arraybuffer';
 
 webSocketServer.on('connection', function connection(ws) {
 	console.log('client connected');
-	ws.send("Hello from server");
 
 	ws.on('message', function incoming(data) {
 		console.log('Server Received: %s', data);
@@ -21,16 +24,4 @@ webSocketServer.on('connection', function connection(ws) {
 		});
 	});
 
-});
-
-// telling it to use static files in the public folder
-app.use(express.static('public'));
-
-// creating a static route to the uuid module
-app.use('/scripts', express.static(__dirname + '/node_modules/uuid/dist/'));
-
-// Set Express routes
-// Setting the home page of web server
-app.get('/', (req, res) => {
-  	res.sendFile(__dirname + '/public/views/chat.html');
 });
