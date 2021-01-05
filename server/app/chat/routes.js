baafac3ece368fac96b212b226d7a87189c0d0bb
Vanessa Bergen <vanessa.bergen1@gmail.com>
2021-01-05 18:53:06 -0500

    Update route to find the chat and return the chat object

--- server/app/chat/routes.js
+++ server/app/chat/routes.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
 	var ctrl = require('./controller.js')();
 	app.post('/chat', ctrl.create);
 	app.get('/chat', ctrl.getAll);
-	//app.get('/chat/:name', ctrl.getByName);
-	app.get('/chat/:name', ctrl.chatExists);
+	app.get('/chat/:name', ctrl.getByName);
+	//app.get('/chat/:name', ctrl.chatExists);
 	console.log('chat routes initialized');
 }
